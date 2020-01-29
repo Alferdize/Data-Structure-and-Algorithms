dbb792e3fbdde14dc3b3105c8220efbcb8f90e7a
Alferdize <simonandrew132@gmail.com>
2020-01-30 00:53:52 +0530

    Problem seven solve

--- problem7/two.py
+++ problem7/two.py
@@ -1,17 +1,21 @@
 def is_char(code):
+    code = int(code)
     return 0 if code > 26 or code < 1 else 1
 
-def get_message_count(code):
-    code_str = str(code)
+
+def get_message_count(code_str):
+    code_str = str(code_str)
     if len(code_str) == 1:
-        count = 1
+        count = is_char(code_str)
     elif len(code_str) == 2:
-        count = 1 + is_char(code)
+        count = 1 + is_char(code_str)
     else:
         count = get_message_count(int(code_str[1:]))
         if is_char(int(code_str[:2])):
             count += get_message_count(int(code_str[2:]))
+
     return count
+
 
 
 assert get_message_count(81) == 1
