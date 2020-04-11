f0b3e56257ccd3e1323aa4375704de36c28abb36
Alferdize <simonandrew132@gmail.com>
2020-04-12 00:34:01 +0530

    Quicksort

---
+++ Algorithms/Quicsort.py
@@ -0,0 +1,27 @@
+def quicsort(A, low, high):
+    if low < high:
+        p = partition(A, low, high)
+        quicsort(A, low, p - 1)
+        quicsort(A, p + 1, high)
+
+
+def partition(A, low, high):
+    pivot = A[high]
+    i = low
+    for j in range(low, high, 1):
+        if A[j] < pivot:
+            temp = A[j]
+            A[j] = A[i]
+            A[i] = temp
+            i += 1
+    temp = A[high]
+    A[high] = A[i]
+    A[i] = temp
+    return i
+
+
+A = [1, 2, 4, 9, 3, 7]
+low = 0
+high = len(A)-1
+quicsort(A, low, high)
+print(A)
