51500800033f8d591cb1f883418bd9e2c309de24
Alferdize <simonandrew132@gmail.com>
2020-05-16 19:23:57 +0530

    Linear Search

--- solve_kickstart/2019/Round B/Bike_Tour.py
+++ solve_kickstart/2019/Round B/Bike_Tour.py
@@ -1,9 +1,9 @@
 T = int(input())
-for i in range(1,T+1):
+for i in range(1, T+1):
     N = int(input())
-    Arr = list(map(int,input().split()))
+    Arr = list(map(int, input().split()))
     out = 0
-    for j in range(1,N-1):
+    for j in range(1, N-1):
         if Arr[j] > Arr[j-1] and Arr[j] > Arr[j+1]:
             out += 1
-    print("Case #{}: {}".format(i,out))
+    print("Case #{}: {}".format(i, out))
