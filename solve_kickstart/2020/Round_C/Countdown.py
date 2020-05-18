fca33c066db134a346978a1c8f31968e08230d1d
Alferdize <simonandrew132@gmail.com>
2020-05-19 00:10:39 +0530

    Contest 7

--- solve_kickstart/2020/Round_C/Countdown.py
+++ solve_kickstart/2020/Round_C/Countdown.py
@@ -2,9 +2,15 @@ T = int(input())
 for i in range(1,T+1):
     N, M = list(map(int,input().split()))
     Arr = list(map(int,input().split()))
-    substring = [i for i in range(M,0,-1)]
     cnt = 0
-    for j in range(N-(M-1)):
-        if Arr[j:j+M] == substring:
-            cnt +=1
+    series = 0
+    for j in range(len(Arr)):
+        if Arr[j] == M:
+            series =1
+        elif series > 0 and j > 0 and Arr[j -1] == Arr[j] + 1:
+            series += 1
+        else:
+            series = 0
+        if series == M:
+            cnt += 1
     print("Case #{}: {}".format(i,cnt))
