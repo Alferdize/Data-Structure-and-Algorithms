24b28f6bfdf91e20b8b20f4f7ec22e546d994bf1
Alferdize <simonandrew132@gmail.com>
2020-05-25 11:57:35 +0530

    Done

---
+++ Hackerrank/Database.py
@@ -0,0 +1,15 @@
+import sys
+ 
+for _ in range(int(sys.stdin.readline())):
+    cad , rad = map(int, sys.stdin.readline().split())
+    d = [list(sys.stdin.readline().split()) for _ in range(rad + 1)]
+    w = [max(a) for a in zip(*(list(map(len, b)) for b in d))]
+    u = '+' + '+'.join('-' * (x + 2) for x in w) + '+'
+    print(u)
+    print('| ' + ' | '.join(d[0][i].ljust(w[i]) for i in range(cad)) + ' |')
+    print(u)
+    for a in d[1:]:
+        print('| ' + ' | '.join(
+            a[i].rjust(w[i]) if a[i][0] in '0123456789' and '/' not in a[i] else a[i].ljust(w[i]) for i in
+            range(cad)) + ' |')
+    print(u)
